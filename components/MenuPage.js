19406b465dac242faa1688f717ea83b3d0d7893b
gervais <gervaisamoah@yahoo.com>
2024-03-18 14:34:11 +0000

    Create a proxy for the Store

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -2,6 +2,15 @@ export class MenuPage extends HTMLElement {
   constructor() {
     super();
     this.root = this.attachShadow({ mode: "open" });
+    const styles = document.createElement("style");
+    this.root.appendChild(styles);
+
+    async function loadCSS() {
+      const request = await fetch("/components/MenuPage.css");
+      const css = await request.text();
+      styles.textContent = css;
+    }
+    loadCSS();
   }
 
   connectedCallback() {
